0b5e7614124bb19959b1d239162294d9aae77e56
Tamas Floch <tamasfloch95@gmail.com>
2019-02-06 18:21:19 +0100

    godpush 7 flies 1 hit

--- reports.py
+++ reports.py
@@ -5,10 +5,10 @@
 def count_games(file_name):
 
     with open(file_name, 'r') as f:
-        counter = 0
+        count = 0
         for line in f:
-            counter += 1
-        return counter
+            count += 1
+        return count
 
 
 def decide(file_name, year):
@@ -18,10 +18,65 @@ def decide(file_name, year):
         for line in f:
             game_info = line.split('\t')
             game_info[4] = game_info[4].strip('\n')
-            print(game_info[2])
             if int(game_info[2]) == year:
                 init_check = 1
         if init_check == 1:
             return True
         else: 
             return False
+
+
+def get_latest(file_name):
+
+    latest_game = ['', 0]
+    with open(file_name, 'r') as f:
+        for line in f:
+            game_info = line.split('\t')
+            game_info[2] = int(game_info[2])
+            if game_info[2] > latest_game[1]: 
+                latest_game[0] = game_info[0]
+                latest_game[1] = game_info[2]
+        return latest_game[0]
+            
+
+def count_by_genre(file_name, genre):
+
+    count = 0
+    with open(file_name, 'r') as f:
+        for line in f:
+            game_info = line.split('\t')
+            game_info[4] = game_info[3].strip('\n')
+            if game_info[4] == genre:
+                count += 1
+        return count
+    
+
+def get_line_number_by_title(file_name, title):
+
+    init_check = 0
+    with open(file_name, 'r') as f:
+        for x,line in enumerate(f):
+            game_info = line.split('\t')
+            if game_info[0] == title:
+                init_check = 1
+                line_number = x + 1
+        if init_check == 1:
+            return line_number
+        else: 
+            raise ValueError
+
+
+def sort_abc(file_name):
+    
+    ordered_titles = []
+    with open(file_name, 'r') as f:
+        for line in f:
+            game_info = line.split('\t')
+            if len(ordered_titles) == 0 or (game_info[0]).lower() > (ordered_titles[len(ordered_titles) - 1]).lower():
+                ordered_titles.append(game_info[0])
+            else:
+                for x, title in enumerate(ordered_titles):
+                    if (game_info[0]).lower() < title.lower():
+                        ordered_titles.insert(x, game_info[0])
+                        break
+        return ordered_titles
