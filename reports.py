ea431b2acecdea9dd6784fbff38c06f057fbe550
krekkPatrik <krekk.patrik@gmail.com>
2019-02-06 12:17:35 +0100

    I definedcount game func.

--- reports.py
+++ reports.py
@@ -1,2 +1,12 @@
 
 # Report functions
+
+
+def count_games(file_name):
+
+    with open('file_name', 'r') as f:
+        counter = 0
+        for line in f:
+            counter += 1
+        return counter
+
